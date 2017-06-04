893c082e3673c9a4070642a01947b7321c85ce85
dannywu0820 <livebetterlife0820@gmail.com>
2017-06-05 00:38:18 +0800

    [feature]: 2-nd version of multiple layer perceptron algorithm

--- Multiple_Layer_Perceptron.py
+++ Multiple_Layer_Perceptron.py
@@ -6,44 +6,20 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-#Step1. prepare dataset
-Labels = []
-Features = []
+def read_dataset(file_location):
+    labels = []
+    features = []
 
-file_location = './Dataset/cross200.txt'
-fp = open(file_location, 'r')
-for line in fp.readlines():
-    line_elements = line.split()
-    Labels.append(int(line_elements.pop()))
-    Features.append([float(i) for i in line_elements])
+    fp = open(file_location, 'r')
+    for line in fp.readlines():
+        line_elements = line.split()
+        labels.append(int(line_elements.pop()))
+        features.append([float(i) for i in line_elements])
 
-dataset = list(zip(Features, Labels))
+    dataset = list(zip(features, labels))
+    random.shuffle(dataset)
 
-#Step2. divide dataset into training and testing set
-#random.shuffle(dataset)
-training_size = int(len(dataset)) #int(len(dataset)*0.8)
-training_set = dataset #dataset[0:training_size]
-random.shuffle(training_set)
-testing_size = int(len(dataset)) #int(len(dataset)) - training_size
-testing_set = dataset[training_size:]
-
-training_features = []
-training_labels = []
-for sample in training_set:
-    #training_features.append([sample[0], sample[1]])
-    #training_labels.append([sample[2]-1])
-    training_features.append(sample[0])
-    training_labels.append([sample[1]-1])
-
-testing_features = []
-testing_labels = []
-#for sample in testing_set:
-for sample in dataset:
-    #testing_features.append([sample[0], sample[1]])
-    #testing_labels.append([sample[2]-1])
-    testing_features.append(sample[0])
-    testing_labels.append(sample[1]-1)
-
+    return dataset
 
 def plot_dataset(dataset):
     label = []
@@ -53,29 +29,14 @@ def plot_dataset(dataset):
         feature1.append(data[0][0])
         feature2.append(data[0][1])
         label.append(data[1])
-    dataset_to_plot = list(zip(feature1, feature2, label))
+    data_to_plot = list(zip(feature1, feature2, label))
+    dataframe_obj = pd.DataFrame(data=data_to_plot, columns=['Feature1', 'Feature2', 'Label'])
+
+    dataframe_obj.plot(kind='scatter', x='Feature1', y='Feature2', c='Label')
     #dataframe_obj = pd.read_csv(file_location, header=None, sep=" ", names=['col1', 'col2', 'col3'])
-    dataframe_obj = pd.DataFrame(data=dataset_to_plot, columns=['feature1', 'feature2', 'label'])
-    dataframe_obj.plot(kind='scatter', x='feature1', y='feature2', c='label')
-    plt.show()
     #print dataframe_obj.info()
     #print dataframe_obj.head(10)
     #print dataframe_obj.tail(10)
-    #print dataframe_obj['feature1']
-
-#Step3. train a neural network
-epochs = 3000           # Number of iterations
-inputLayerSize, hiddenLayerSize, outputLayerSize = 2, 10, 1
-
-#X = np.array([[0,0], [0,1], [1,0], [1,1]])
-#Y = np.array([ [0],   [1],   [1],   [0]])
-X = np.array(training_features)
-Y = np.array(training_labels)
-J = []
-
-def sigmoid (x): return 1/(1 + np.exp(-x))      # activation function
-def sigmoid_(x): return x * (1 - x)             # derivative of sigmoid #return sigmoid(x) * (1 - sigmoid(x))
-                                                # weights on layer inputs
 
 def activation(x, type='sigmoid', derivative=False):
 	if type == 'sigmoid':
@@ -89,75 +50,160 @@ def activation(x, type='sigmoid', derivative=False):
 	else:
 		pass
 
-Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
-Wz = np.random.uniform(size=(hiddenLayerSize,outputLayerSize))
+def build_model(training_set, structure, epoch=10, rho=0.1, alpha=0.1): #0 <= alpha < 1
+    #inputLayerSize, hiddenLayerSize, outputLayerSize = 2, 8, 1
+    inputLayerSize, hiddenLayerSize, outputLayerSize = structure['Input'], structure['Hidden'], structure['Output']
+    training_features = []
+    training_labels = []
+    for sample in training_set:
+        training_features.append(sample[0])
+        training_labels.append([sample[1]-1])
+    X = np.array(training_features) #X = np.array([[0,0], [0,1], [1,0], [1,1]])
+    Y = np.array(training_labels)   #Y = np.array([ [0],   [1],   [1],   [0]])
+    J = []
 
-#training
-learning_rate = 0.25
-alpha = 0.01 #used in momentum
-for i in range(epochs):
-    prev_Wz = Wz
-    prev_Wh = Wh
-    #forward propagation
-    H = activation(np.dot(X, Wh))                  # hidden layer results
-    Z = activation(np.dot(H, Wz))                  # output layer results
-    E = Y - Z                                   # how much we missed (error)
+    Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
+    Wz = np.random.uniform(size=(hiddenLayerSize,outputLayerSize))
+
+    for i in range(epoch):
+        prev_Wz = Wz
+        prev_Wh = Wh
+        #forward propagation
+        H = activation(np.dot(X, Wh)) # hidden layer results
+        Z = activation(np.dot(H, Wz)) # output layer results
+        E = Y - Z                     # how much we missed (error)
+
+        #backpropagation
+        #w(t+1) = w(t) - (rho * dJ/dw) + (alpha * [w(t)-w(t-1)])
+        dZ = E * activation(np.dot(H, Wz), derivative=True)                        #delta Z
+        dH = dZ.dot(Wz.T) * activation(np.dot(X, Wh), derivative=True)             #delta H
+        dZ = H.T.dot(dZ)
+        dH = X.T.dot(dH)
+        Wz += ((rho * dZ) + alpha * (Wz - prev_Wz)) #update output layer weights
+        Wh += ((rho * dH) + alpha * (Wh - prev_Wh)) #update hidden layer weights
+
+        sum_of_squared_error = 0
+        for error in E:
+            sum_of_squared_error += (error[0]*error[0])                          #update hidden layer weights
+        J.append(sum_of_squared_error)
+
+    x = list(range(epoch))
+    plt.figure(1)
+    plt.xlabel('Epoch')
+    plt.ylabel('Sum of Squared Error')
+    plt.plot(x, J, 'b--')
+
+    model = {'Wz': Wz, 'Wh': Wh, 'Learning_Curve': J}
+    return model
+
+def validate_model(validation_set, model):
+    validation_features = []
+    validation_labels = []
+    for sample in validation_set:
+        validation_features.append(sample[0])
+        validation_labels.append(sample[1]-1)
+
+    X = np.array(validation_features) #X = np.array([[0,0], [0,1], [1,0], [1,1]])
+    Y = np.array(validation_labels)   #Y = np.array([ [0],   [1],   [1],   [0]])
+
+    H = activation(np.dot(X, model['Wh']))
+    Z = activation(np.dot(H, model['Wz']))
+    E = Y - Z
+
     sum_of_squared_error = 0
     for error in E:
         sum_of_squared_error += (error[0]*error[0])
+    model['ValidationError'] = sum_of_squared_error
 
-    #backpropagation
-    #w(t+1) = w(t) - learning_rate * dJ/dw
-	#dZ = E * sigmoid_(Z)                        # delta Z
-	#dH = dZ.dot(Wz.T) * sigmoid_(H)             # delta H
-    dZ = E * activation(np.dot(H, Wz), derivative=True)                        # delta Z
-    dH = dZ.dot(Wz.T) * activation(np.dot(X, Wh), derivative=True)             # delta H
-    dZ = H.T.dot(dZ)
-    dH = X.T.dot(dH)
-    Wz += (learning_rate*dZ + alpha*(Wz - prev_Wz))                          # update output layer weights
-    Wh += (learning_rate*dH + alpha*(Wh - prev_Wh))                           # update hidden layer weights
-    J.append(sum_of_squared_error)
-print sum_of_squared_error/training_size
+def cross_validation(training_data, k=4): #k-fold cross validation
+    fold_size = int(len(training_data)/k)
+    training_folds = []
 
-#print(Z)
-#testing
-testX = np.array(testing_features)
-testY = np.array(testing_labels)
+    for i in range(0, k):
+        new_fold = []
+        for j in range(fold_size*i, fold_size*(i+1)):
+            new_fold.append(training_data[j])
+        training_folds.append(new_fold)
 
-H = sigmoid(np.dot(testX, Wh))                  # hidden layer results
-Z = sigmoid(np.dot(H, Wz))                  # output layer results
-E = testY - Z
-for error in E:
-    sum_of_squared_error += (error[0]*error[0])
-print sum_of_squared_error/testing_size
+    errors = []
+    min_error = sys.maxint
+    best_model = None
+    for i in range(0, k):
+        validation_set = training_folds[0]
+        training_set = training_folds[1] + training_folds[2] + training_folds[3]
 
-x = list(range(epochs))
-plt.xlabel('Epoch')
-plt.ylabel('Sum of Squared Error')
-plt.plot(x, J, 'b--')
-plt.show()
+        #train a model
+        structure = {'Input': 2, 'Hidden': 16, 'Output': 1}
+        epoch, learning_rate, momentum_rate = 1000000, 0.1, 1
+        model = build_model(training_set, structure, epoch, learning_rate, momentum_rate)
+        validate_model(validation_set, model)
+        errors.append(model['ValidationError'])
+        if(model['ValidationError'] < min_error):
+            min_error = model['ValidationError']
+            best_model = model
 
-test_labels = []
-for result in Z:
-    if(result[0] > 0.5):
-        test_labels.append(1)
-    else:
-        test_labels.append(0)
-print test_labels
-x1 = []
-y1 = []
-x2 = []
-y2 = []
-#for i in range(testing_size):
-for i in range(int(len(dataset))):
-    if(test_labels[i] == 1):
-        x1.append(testX[i][0])
-        y1.append(testX[i][1])
-    else:
-        x2.append(testX[i][0])
-        y2.append(testX[i][1])
-plt.scatter(x1, y1, color='r')
-plt.scatter(x2, y2, color='g')
-plt.show()
+        first_fold = training_folds.pop(0)
+        training_folds.append(first_fold)
 
-plot_dataset(dataset)
+    print 'Validation Errors for Hidden Layer Size = ' + str(structure['Hidden'])
+    print errors
+
+    return best_model
+
+def testing(testing_set, model):
+    testing_features = []
+    testing_labels = []
+    for sample in testing_set:
+        testing_features.append(sample[0])
+        testing_labels.append(sample[1]-1)
+
+    X = np.array(testing_features) #X = np.array([[0,0], [0,1], [1,0], [1,1]])
+    target_Y = np.array(testing_labels)   #Y = np.array([ [0],   [1],   [1],   [0]])
+
+    H = activation(np.dot(X, model['Wh']))
+    Z = activation(np.dot(H, model['Wz']))
+
+    estimated_Y = []
+    for result in Z:
+        #print result
+        if(result[0] > 0.5):
+            estimated_Y.append(1)
+        else:
+            estimated_Y.append(0)
+
+    class1 = {'x': [], 'y': []}
+    class2 = {'x': [], 'y': []}
+    correctness = 0
+    for i in range(int(len(testing_set))):
+        if(estimated_Y[i] == target_Y[i]):
+            correctness+=1
+
+        if(estimated_Y[i] == 1):
+            class1['x'].append(X[i][0])
+            class1['y'].append(X[i][1])
+        else:
+            class2['x'].append(X[i][0])
+            class2['y'].append(X[i][1])
+    print 'Correctness: ' + str(float(correctness)*100/float(len(testing_set))) + '%'
+
+    plt.figure(3)
+    plt.scatter(class1['x'], class1['y'], color='r')
+    plt.scatter(class2['x'], class2['y'], color='b')
+    plt.show()
+
+if __name__ == '__main__':
+    dataset = read_dataset('./Dataset/cross200.txt')
+    #print dataset
+
+    training_size = int(len(dataset) * 0.8)
+    testing_size = int(len(dataset) - training_size)
+    training_data = dataset[0:training_size]
+    testing_set = dataset #dataset[training_size:]
+
+    #for model selection
+    best_model = cross_validation(training_data)
+
+    plot_dataset(dataset)
+    testing(testing_set, best_model)
+
+    plt.show()
